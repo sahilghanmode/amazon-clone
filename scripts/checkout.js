b2968547b6af89351521ddd6a8af513c1158195b
sahilghanmode <sahilghanmode1903@gmail.com>
2024-06-19 11:13:10 +0530

    delete the product using DOM

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -18,7 +18,7 @@ cart.forEach((cartItem,index)=>{
 
 
     cartSummaryHTML+=`
-    <div class="cart-item-container">
+    <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
         <div class="delivery-date">
             Delivery date: Tuesday, June 21
         </div>
@@ -106,5 +106,9 @@ document.querySelectorAll('.js-delete-link')
             const productId=link.dataset.productId;
             console.log(productId);
             removeFromCart(productId);
+
+            const container=document.querySelector(`.js-cart-item-container-${productId}`);
+
+            container.remove();
         });
     });
