3ad07b2414b9009113bd3a4fa01f4b49d806d516
sahilghanmode <sahilghanmode1903@gmail.com>
2024-06-20 13:33:59 +0530

    Make delivery options interactive

--- data/cart.js
+++ data/cart.js
@@ -53,3 +53,16 @@ export function removeFromCart(productId){
 
     saveToStorage();
 }   
+
+export function updateDeliveryOption(productId,deliveryOptionId){
+    let matchingItem;
+    cart.forEach((cartItem)=>{
+        if(productId===cartItem.productId){
+            matchingItem=cartItem;
+        }
+    });
+
+    matchingItem.deliveryOptionsId=deliveryOptionId;
+
+    saveToStorage();
+}
