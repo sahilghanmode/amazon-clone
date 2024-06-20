3f98ec1995715e51101e4e8f81f6b36c443ea901
sahilghanmode <sahilghanmode1903@gmail.com>
2024-06-21 01:00:54 +0530

    Create automated tests for formatCurrency

---
+++ tests/moneytest.js
@@ -0,0 +1,17 @@
+import {formatCurrency} from "../scripts/utils/money.js";
+console.log('test suite:currency'); 
+if(formatCurrency(2095)==='20.95'){
+    console.log('passed');
+}else console.log('failed');
+
+if(formatCurrency(0)==='0.00'){
+    console.log('passed');
+}else console.log('failed');
+
+if(formatCurrency(2000.5)==='20.01'){
+    console.log('passed');
+}else console.log('failed')
+
+if(formatCurrency(2000.4)==='20.00'){
+    console.log('passed');
+}else console.log('failed');
