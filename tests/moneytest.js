beb447d2c13d47c1cc43fa10cfb0ce4ff806a682
sahilghanmode <sahilghanmode1903@gmail.com>
2024-06-21 15:12:04 +0530

    Remove deault tests and add money tests

--- tests/moneytest.js
+++ tests/moneytest.js
@@ -1,5 +1,7 @@
 import {formatCurrency} from "../scripts/utils/money.js";
-console.log('test suite:currency'); 
+console.log('test suite:formatCurrency'); 
+
+console.log('converts cents into dollars');
 if(formatCurrency(2095)==='20.95'){
     console.log('passed');
 }else console.log('failed');
