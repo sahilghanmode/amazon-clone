6b03bf5711918ec7059b492d205b9ac9bdbae190
sahilghanmode <sahilghanmode1903@gmail.com>
2024-06-21 23:30:21 +0530

    Create automated tests for addToCart

---
+++ tests-jasmine/data/cartTest.js
@@ -0,0 +1,42 @@
+import {cart,addToCart,loadFromStorage, updateDeliveryOption} from '../../data/cart.js';
+
+
+describe('test suit: addToCart',()=>{
+
+
+    it('adds an existing product to the cart',()=>{
+
+        spyOn(localStorage,'setItem');
+
+        spyOn(localStorage,'getItem').and.callFake(()=>{
+            return JSON.stringify([{
+                productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+                quantity:1,
+                deliveryOptionsId:'1'
+            }]);
+        });
+
+        loadFromStorage();
+
+        addToCart('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
+        expect(cart.length).toEqual(1);
+        expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+
+    });
+
+    it('adds a new product to the cart',()=>{
+        spyOn(localStorage,'setItem'); 
+
+        spyOn(localStorage,'getItem').and.callFake(()=>{
+            return JSON.stringify([]);
+        });
+
+        loadFromStorage();
+
+        addToCart('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
+        expect(cart.length).toEqual(1);
+        expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+
+    });
+
+});
