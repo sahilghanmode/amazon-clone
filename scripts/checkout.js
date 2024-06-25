524ed384a3c29ebd875e8cee6ebf06979f43a58b
sahilghanmode <sahilghanmode1903@gmail.com>
2024-06-25 23:16:14 +0530

    dont know what i did but to keep streak alive

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,7 +1,17 @@
+import { loadProducts } from "../data/products.js";
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 //import '../data/cart-class.js';
 //import "../data/backend-practice.js"
 
-renderOrderSummary();
-renderPaymentSummary();
+new Promise(()=>{
+    loadProducts(()=>{
+        resolve();
+    })
+})
+
+loadProducts(()=>{
+
+    renderOrderSummary();
+    renderPaymentSummary();
+});
